d9728fb1b31598417630c8cfcc4e3a467254651f
Simas Cepaitis <simas.cepaitis@pearson.com>
2017-01-17 14:39:14 +0000

    Add validator cmd

--- cmd/validator/main.go
+++ cmd/validator/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/pearsontechnology/environment-operator/version"
 )
 
+// This package adds environment-validator binary, which can be used to
+// validate environments.bitesize file
 func main() {
 	fmt.Println(version.Version)
 }
--- version/version.go
+++ version/version.go
@@ -1,3 +1,4 @@
 package version
 
+// Version for environment-operator
 var Version = "0.0.1+git"
